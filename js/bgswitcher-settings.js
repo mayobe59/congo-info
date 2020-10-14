9425763e39e0bc66b5f44afc27a5b22dbc582c8f
mayobe59 <mayosbt199759@gmail.com>
2020-10-14 09:39:47 +0900

    firstcommit

---
+++ js/bgswitcher-settings.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+    $('header-none').bgSwitcher({
+      images: [
+		  "./images/images-top/image_fabric.png", "./images/images-top/image_river_2.JPG", "./images/images-top/image_town_2.png", "./images/images-top/image_river_flag.png", "./images/images-top/image_river_1.png", "./images/images-top/image_town_1.png"],
+      interval: 7000,
+    });
+});
