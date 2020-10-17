2471fc3ed75eef8fc2572c6ac42d07b22c39830b
mayobe59 <mayosbt199759@gmail.com>
2020-10-17 16:53:43 +0900

    5thcommit

--- js/bgswitcher-settings.js
+++ js/bgswitcher-settings.js
@@ -6,3 +6,15 @@ jQuery(function($) {
       interval: 7000,
     });
 });
+
+/*
+
+jQuery(function($) {
+    $('.bg-slider').bgSwitcher({
+      images: [
+		  "./images/images-top/top-1.png", "./images/images-top/top-2.png", "./images/images-top/top-3.png","./images/images-top/top-4.png","./images/images-top/top-5.png",
+	  ],
+      interval: 7000,
+    });
+});
+*/
